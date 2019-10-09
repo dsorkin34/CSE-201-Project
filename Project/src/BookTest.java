2f5fbc45495c93346cd68dfb83d52c98fee28a04
Ashley <54953778+beyap@users.noreply.github.com>
2019-10-09 17:24:36 -0400

    Update and rename TestDriver.java to BookTest.java

---
+++ Project/src/BookTest.java
@@ -0,0 +1,55 @@
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookTest {
+
+	Book b1 = new Book("title1", "name here", "descriptions here", "genreListed");
+	Book b2 = new Book("bookName", "testing name", "cool description", "theGenre");
+	
+	@Test
+	public void testBook() {
+		// String title, String name, String description, String genre
+		assertEquals(b1.toString(), "Book [title=title1, Name=name here, description=descriptions here, genre=genreListed]");
+		assertEquals(b2.toString(), "Book [title=bookName, Name=testing name, description=cool description, genre=theGenre]");
+	}
+	
+	@Test
+	public void testGetTitle() {
+		assertEquals(b1.getTitle(), "title1");
+		assertEquals(b2.getTitle(), "bookName");
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals(b1.getName(), "name here");
+		assertEquals(b2.getName(), "testing name");
+	}
+	
+	@Test
+	public void testGetfName() {
+		assertEquals(b1.getfName(), "name");
+		assertEquals(b2.getfName(), "testing");
+	}
+	
+	@Test
+	public void testGetlName() {
+		assertEquals(b1.getlName(), "here");
+		assertEquals(b2.getlName(), "name");
+	}
+	
+	@Test
+	public void testGetDescription() {
+		assertEquals(b1.getDescription(), "descriptions here");
+		assertEquals(b2.getDescription(), "cool description");
+	}
+	
+	@Test
+	public void testGetGenre() {
+		assertEquals(b1.getGenre(), "genreListed");
+		assertEquals(b2.getGenre(), "theGenre");
+	}
+
+}
+
--- Project/src/TestDriver.java
+++
@@ -1,9 +0,0 @@
-
-public class TestDriver {
-	public static void main(String[] args) {
-		String a = "test";
-		System.out.println(a.contains("t"));
-		Book bookTest = new Book("title", "name name", "description", "genre");
-		System.out.println(bookTest);
-	}
-}
