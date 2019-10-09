58eaa02e5d54abee3935cd5189de3632b5c41a03
cwrighter <54953445+cwrighter@users.noreply.github.com>
2019-10-09 17:38:34 -0400

    Update LocalBookShelfTest.java

--- Project/src/LocalBookShelfTest.java
+++ Project/src/LocalBookShelfTest.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.Test;
 
 class LocalBookShelfTest {
 
+	
+	// Test the add book method to see if book was added successfully 
+	// Need to test for duplicates in the arryList of books 
+	// Testing to see if number of books was incremented by one everytime book is added
 	@Test
 	void testAddBook() {
+		// Create an Object
+		 Book harryPotter = new Book();
 		fail("Not yet implemented");
 	}
 
@@ -34,6 +40,9 @@ class LocalBookShelfTest {
 		fail("Not yet implemented");
 	}
 
+	
+	// Testing the sorting by title method to see if when choosing to sort by title, will arrayList of books be in sorted order
+	// Test to see if lower case and upper case effect the sort 
 	@Test
 	void testSortByTitle() {
 		fail("Not yet implemented");
