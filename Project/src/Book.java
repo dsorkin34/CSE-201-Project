6a6bab58747fe3a574dc6b148a9019f04fd48788
XU-Nuo <49825669+XU-Nuo@users.noreply.github.com>
2019-10-09 16:26:02 -0400

    Update Book.java

--- Project/src/Book.java
+++ Project/src/Book.java
@@ -1,14 +1,31 @@
-
+/*
+ * Author: Nuo XU
+ * the date: 09/26/2019
+ * This is the Book class for a single Book
+ */
 public class Book {
+	// ======================= Property
 	private String title;
-	private String fName;
-	private String lName;
-	private String description;
-	private String genre;
-	
+	private String fName; // The first name of the author
+	private String lName; // The last name of the author
+	private String description; // The description of the book, must be paragraph
+	private String genre; // The genre of the book
+
+	// ======================= Constructor
+	// ..Null Constructor
 	public Book() {
 	}
 
+	/**
+	 * The Main Constructor
+	 * 
+	 * @param title       The title of the book
+	 * @param name        The author's name, first name and last name only,
+	 *                    seperated by a space
+	 * @param description The description of the book, must be paragraph (no return
+	 *                    or line feed character)
+	 * @param genre       The genre of the book
+	 */
 	public Book(String title, String name, String description, String genre) {
 		this.title = title;
 		this.fName = name.split("\\s")[0];
@@ -17,12 +34,24 @@ public class Book {
 		this.genre = genre;
 	}
 
+	// ======================= Method
+	/**
+	 * Override the stock toString method
+	 * 
+	 * @return All attributes of a Book object
+	 */
 	@Override
 	public String toString() {
 		return "Book [title=" + title + ", Name=" + getName() + ", description=" + description + ", genre=" + genre
 				+ "]";
 	}
 
+	// ======================= Getter & Setter
+	/**
+	 * Get the title of a Book object
+	 * 
+	 * @return The tile of a Book
+	 */
 	public String getTitle() {
 		return title;
 	}
@@ -31,6 +60,11 @@ public class Book {
 		this.title = title;
 	}
 
+	/**
+	 * Get the author's name of a Book object
+	 * 
+	 * @return The author's first name and last name seperated by a space
+	 */
 	public String getName() {
 		return fName + " " + lName;
 	}
@@ -40,6 +74,11 @@ public class Book {
 		this.lName = Name.split("\\s")[1];
 	}
 
+	/**
+	 * Get the author's first name of a Book object
+	 * 
+	 * @return The author's first name
+	 */
 	public String getfName() {
 		return fName;
 	}
@@ -48,6 +87,11 @@ public class Book {
 		this.fName = fName;
 	}
 
+	/**
+	 * Get the author's last name of a Book object
+	 * 
+	 * @return The author's last name
+	 */
 	public String getlName() {
 		return lName;
 	}
@@ -56,6 +100,11 @@ public class Book {
 		this.lName = lName;
 	}
 
+	/**
+	 * Get the description of a Book object
+	 * 
+	 * @return The one-paragraph description of a Book
+	 */
 	public String getDescription() {
 		return description;
 	}
@@ -64,6 +113,11 @@ public class Book {
 		this.description = description;
 	}
 
+	/**
+	 * Get the genre of a Book object
+	 * 
+	 * @return The genre of a Book
+	 */
 	public String getGenre() {
 		return genre;
 	}
