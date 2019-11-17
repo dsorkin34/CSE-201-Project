049abef48f8d8a78719af76c3bc211bf4cd9e4ad
Ashley <54953778+beyap@users.noreply.github.com>
2019-11-17 12:43:42 -0500

    Added tests for ratings/comments

--- Project/src/BookTest.java
+++ Project/src/BookTest.java
@@ -3,6 +3,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.AssertionFailedError;
+
 public class BookTest {
 
 	Book b1 = new Book("92883", "title1", "name here", "descriptions here", "genreListed");
@@ -10,8 +12,34 @@ public class BookTest {
 
 	@Test
 	public void testToString() {
-		assertEquals(b1.toString(),"Book\nISBN   = 92883\ntitle       = title1\nname        = name here\ndescription = descriptions here\ngenre       = genreListed\n");
-		assertEquals(b2.toString(), "Book\nISBN   = 92729\ntitle       = bookName\nname        = testing name\ndescription = cool description\ngenre       = theGenre\n");
+		b1.addRating(3.0);
+		b2.addRating(2.0);
+		b2.addRating(1.0);
+		b1.addComment("userName", "content");
+		b2.addComment("userName", "content");
+		
+		assertEquals(b1.toString(),
+		"Book\n" + 
+		"ISBN        = " + "92883"                 + "\n"+
+		"title       = " + "title1"                + "\n"+
+		"name        = " + "name here"             + "\n"+
+		"description = " + "descriptions here"     + "\n"+
+		"genre       = " + "genreListed"           + "\n"+
+		"Rating      = " + "3.0"                   + "\n"+
+		"Rating Count= " + "1"                     + "\n"+
+		"Comments    = " + "{userName=content}"    + "\n");
+		
+		assertEquals(b2.toString(),
+		"Book\n" + 
+		"ISBN        = " + "92729"                 + "\n"+
+		"title       = " + "bookName"              + "\n"+
+		"name        = " + "testing name"          + "\n"+
+		"description = " + "cool description"      + "\n"+
+		"genre       = " + "theGenre"              + "\n"+
+		"Rating      = " + "1.5"                   + "\n"+
+		"Rating Count= " + "2"                     + "\n"+
+		"Comments    = " + "{userName=content}"    + "\n");
+		
 	}
 
 	@Test
@@ -72,6 +100,43 @@ public class BookTest {
 		assertEquals(b1.getGenre(), "genreListed");
 		assertEquals(b2.getGenre(), "theGenre");
 	}
-
+	
+	@Test
+	public void testGetRating() {
+		b1.addRating(3.0);
+		b2.addRating(2.0);
+		b2.addRating(1.0);
+		assertEquals(b1.getRating(), 3.0, 0);
+		assertEquals(b2.getRating(), 1.5, 0);
+	}
+	
+	@Test
+	public void testAddRating() {
+		b1.addRating(3.0);
+		b1.addRating(1.0);
+		b2.addRating(2.0);
+		b2.addRating(2.0);
+		b2.addRating(5.0);
+		assertEquals(b1.getRating(), 2.0, 0);
+		assertEquals(b2.getRating(), 3.0, 0);
+	}
+	
+	@Test
+	public void testGetComments() {
+		assertEquals(b1.getComments().toString(), "{}");
+		assertEquals(b2.getComments().toString(), "{}");
+		b1.addComment("userName", "content");
+		b2.addComment("userName2", "content2");
+		assertEquals(b1.getComments().toString(), "{userName=content}");
+		assertEquals(b2.getComments().toString(), "{userName2=content2}");
+		b1.addComment("userName3", "content3");
+		assertEquals(b1.getComments().toString(), "{userName=content, userName3=content3}");
+		assertNotEquals(b2.getComments().toString(), "{userName=content, userName3=content3}");
+	}
+	
+	@Test
+	public void testAddComments() {
+		assertTrue(b1.addComment("userName", "content"));
+		assertTrue(b2.addComment("userName2", "content2"));
+	}
 }
-
